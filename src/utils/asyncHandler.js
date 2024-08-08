22b534a07a0db57a1c9621336853a53f43779879
seeker53 <dishant.singh16@gmail.com>
2024-08-08 21:39:53 +0530

    getting started with controllers and routes

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,24 +1,27 @@
-// const asyncHandler = (requestHandler)=>{
-//     Promise.resolve(requestHandler(req,res,next))
-//     .catch((err)=>next(err))
-// }
-
-const asyncHandler = (requestHandler) =>(req,res,next) =>{
-    new Promise((resolve, reject)=>{
-        requestHandler(req,res,next).then(resolve).catch(reject);
-    })
-    .catch((err)=> next(err))
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
 }
 
-// const asyncHandler = (fn) => async (req,res,next)=>{
-//     try{
-//         await fn(req,res,next)
-//     } catch(error){
-//         res.status(error.code || 500).json({
-//             success:false,
-//             message:error.message
+
+export { asyncHandler }
+
+
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
 //         })
 //     }
 // }
-
-export {asyncHandler}
