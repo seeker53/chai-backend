2e795bf4008418dd19f0e4f44c744e55428a8f6c
seeker53 <dishant.singh16@gmail.com>
2024-08-16 23:44:51 +0530

    added routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { 
+    registerUser, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory 
+} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const userRouter = Router();
@@ -28,5 +40,19 @@ userRouter.route("/login").post(loginUser)
 userRouter.route("/logout").post(verifyJWT,logoutUser);
 
 userRouter.route("/refresh-token").post(refreshAccessToken);
+
+userRouter.route("/change-password").post(verifyJWT,changeCurrentPassword);
+
+userRouter.route("/current-user").get(verifyJWT, getCurrentUser);
+
+userRouter.route("/update-account").patch(verifyJWT, updateAccountDetails); // we are using patch for update beacuse if we use post then all details will be unnecessary updated
+
+userRouter.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar); // first middleware is verifyJWT because we want only the logged-in users to upload files
+
+userRouter.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage);
+
+userRouter.route("/channel/:username").get(verifyJWT,getUserChannelProfile);
+
+userRouter.route("/history").get(verifyJWT,getWatchHistory);
 
 export default userRouter;
